56cb1b8e85f5a6e8df06cfdb4e360a16ac60b447
Abhijith K <95738942+Ak-abhi@users.noreply.github.com>
2022-03-21 13:52:09 +0530

    Add files via upload

---
+++ Div.java
@@ -0,0 +1,10 @@
+class Div{
+	public static void main(String args[]){
+		int a=30;
+		int b=10;
+		double q=a/b;
+		int r=a%b;
+		System.out.println("The quotient is:"+q);
+		System.out.println("The remainder is:"+r);
+	}
+}
---
+++ Multi.java
@@ -0,0 +1,8 @@
+class Multi{
+	public static void main(String args[]){
+		float a=2.5f;
+		float b=4.2f;
+		float m=a*b;
+		System.out.println("The product is:"+m);
+	}
+}
